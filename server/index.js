ecdb91cba025aad1cc116645e2b447a0ff991d4b
haibin-gua <1305263409@qq.com>
2020-05-09 20:01:53 +0800

    admin

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+app.use(require('cors')())
+app.use(express.json())
+mongoose.connect('mongodb://localhost:27017/element-admin',{
+    useNewUrlParser:true,
+    useFindAndModify:true,
+    useCreateIndex:true
+})
+const Article = mongoose.model('Article',new mongoose.Schema({
+    title:{type:String},
+    body:{type:String},
+})) 
+
+
+
+app.get('/',async(req,res)=>{
+    res.send('index')
+})
+//新增文章
+app.post('/api/articles',async(req,res)=>{
+    const articles = await Article.create(req.body)
+    res.send(articles)
+})
+//文章列表
+app.get('/api/articles',async(req,res)=>{
+    const articles = await Article.find()
+    res.send(articles)
+})
+//删除文章
+app.delete('/api/articles/:id',async(req,res)=>{
+    await Article.findByIdAndDelete(req.params.id)
+    res.send({
+        status:true
+    })
+})
+//文章详情
+app.get('/api/articles/:id',async(req,res)=>{
+    const articles = await Article.findById(req.params.id)
+    res.send(articles)
+})
+//修改文章
+app.put('/api/articles/:id',async(req,res)=>{
+    const articles = await Article.findByIdAndUpdate(req.params.id,req.body)
+    res.send(articles)
+})
+
+app.listen('3000',()=>{
+    console.log('http://localhots:3000/');
+});
